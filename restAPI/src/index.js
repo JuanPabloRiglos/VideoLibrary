d1c2bb5fed36966991bd6497dfed57f0a38c54d8
Juan Pablo Riglos <juan.riglos@hotmail.com>
2024-04-03 16:11:17 -0300

    Borre el index.js, cambie el index.ts por .js, removi el build de scripts,todo para intento deploy

--- restAPI/src/index.js
+++ restAPI/src/index.js
@@ -1,2 +1,11 @@
-"use strict";
-const hello = 'hola mundo';
+import dotenv from 'dotenv'
+import app from "./app";
+import "./database"
+
+dotenv.config()
+async function main(){
+    await app.listen(app.get('port'))
+    console.log(`runing in port ${app.get('port')}`)
+}
+
+main()
